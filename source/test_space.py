e97ab4cae3575ea1b81f4308b5346e9d36046720
Stephen Margheim <margheim@sas.upenn.edu>
2014-06-12 12:48:37 -0400

    clean up & organize

---
+++ source/test_space.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# encoding: utf-8
+from __future__ import unicode_literals
+
+import bundler
+bundler.init()
+import pyzotero
+print pyzotero
+
+
+
+def main():
+    pass
+    
+
+    
+if __name__ == '__main__':
+    main()
