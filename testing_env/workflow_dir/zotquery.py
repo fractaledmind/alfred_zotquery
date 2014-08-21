9a1a98ca68b5c183541cf1098a0e44742157befe
Stephen Margheim <stephen.margheim@gmail.com>
2014-08-21 01:20:46 -0400

    add ZotAPI
    ZotWorkflow now uses ZotAPI instead of pyzotero

--- testing_env/workflow_dir/zotquery.py
+++ testing_env/workflow_dir/zotquery.py
@@ -1193,6 +1193,224 @@ class ZotQuery(object):
             all_notes.append(note[33:-10])
         return all_notes
 
+#-------------------------------------------------------------------------------
+# :class:`ZotAPI` ------------------------------------------------------------
+#-------------------------------------------------------------------------------
+
+class ZotAPI(object):
+    """
+    Zotero API (v.3) methods
+    """
+    def __init__(self, library_id=None, library_type=None, api_key=None):
+        """ Store Zotero credentials
+        """
+        self.base = 'https://api.zotero.org'
+        if library_id and library_type:
+            self.library_id = library_id
+            if library_type in ('user', 'group'):
+                self.library_type = library_type + 's'
+            else:
+                raise RuntimeError('Invalid library type: %s' % library_type)
+        if api_key:
+            self.api_key = api_key
+        self.request = None
+        self.links = None
+
+    def _retrieve_data(self, request=None, **kwargs):
+        """
+        Retrieve Zotero items via the API
+        Combine endpoint and request to access the specific resource
+        Returns an JSON object
+        """
+        full_url = '{}{}'.format(self.base, request)
+        headers = {'User-Agent': "ZotQuery/{}".format(__version__),
+                   'Authorization': "Bearer {}".format(self.api_key),
+                   'Zotero-API-Version': 3}
+        kwargs.update({'format': 'json'})
+        self.request = web.get(url=full_url,
+                               headers=headers,
+                               params=kwargs)
+        self.links = self._extract_links()
+        self.request.raise_for_status()
+        return self.request.json()
+
+    def _prep_url(self, url, var=None):
+        if var == None:
+            return url.format(t=self.library_type,
+                              u=self.library_id)
+        else:
+            return url.format(t=self.library_type,
+                              u=self.library_id,
+                              x=var)
+
+    def _extract_links(self):
+        try:
+            links = self.request.headers['link']
+            link_data = re.findall(r'<(.*?)>;\srel="(.*?)"', links)
+            extracted = {}
+            for link in link_data:
+                url, key = link
+                extracted[key] = url
+            return extracted
+        except KeyError:
+            return None
+
+
+    # No argument  -------------------------------------------------------------
+
+    def items(self, **kwargs):
+        """Get items from Zotero
+
+        :rtype: ``list``
+        """
+        url = self._prep_url("/{t}/{u}/items")
+        return self._retrieve_data(url, **kwargs)
+
+    def top_items(self, **kwargs):
+        """Get items from Zotero
+
+        :rtype: ``list``
+        """
+        url = self._prep_url("/{t}/{u}/items/top")
+        return self._retrieve_data(url, **kwargs)
+
+    def trash_items(self, **kwargs):
+        """Get items from Zotero
+
+        :rtype: ``list``
+        """
+        url = self._prep_url("/{t}/{u}/items/trash")
+        return self._retrieve_data(url, **kwargs)
+
+    def tags(self, **kwargs):
+        """Get items from Zotero
+
+        :rtype: ``list``
+        """
+        url = self._prep_url("/{t}/{u}/tags")
+        return self._retrieve_data(url, **kwargs)
+
+    def collections(self, **kwargs):
+        """Get items from Zotero
+
+        :rtype: ``list``
+        """
+        url = self._prep_url("/{t}/{u}/collections")
+        return self._retrieve_data(url, **kwargs)
+
+    def top_collections(self, **kwargs):
+        """Get items from Zotero
+
+        :rtype: ``list``
+        """
+        url = self._prep_url("/{t}/{u}/collections/top")
+        return self._retrieve_data(url, **kwargs)
+
+    # Requires Argument  -------------------------------------------------------
+
+    def item(self, item_id, **kwargs):
+        """Get items from Zotero
+
+        :rtype: ``dict``
+        """
+        url = self._prep_url("/{t}/{u}/items/{x}", item_id)
+        return self._retrieve_data(url, **kwargs)
+
+    def collection(self, collection_id, **kwargs):
+        """Get items from Zotero
+
+        :rtype: ``dict``
+        """
+        url = self._prep_url("/{t}/{u}/collections/{x}", collection_id)
+        return self._retrieve_data(url, **kwargs)
+
+    ## -------------------------------------------------------------------------
+
+    def item_children(self, item_id, **kwargs):
+        """Get items from Zotero
+
+        :rtype: ``list``
+        """
+        url = self._prep_url("/{t}/{u}/items/{x}/children", item_id)
+        return self._retrieve_data(url, **kwargs)
+
+    def item_tags(self, item_id, **kwargs):
+        """Get items from Zotero
+
+        :rtype: ``list``
+        """
+        url = self._prep_url("/{t}/{u}/items/{x}/tags", item_id)
+        return self._retrieve_data(url, **kwargs)
+
+    def tag(self, tag_name, **kwargs):
+        """Get items from Zotero
+
+        :rtype: ``list``
+        """
+        url = self._prep_url("/{t}/{u}/tags/{x}", tag_name)
+        return self._retrieve_data(url, **kwargs)
+
+    def collection_children(self, collection_id, **kwargs):
+        """Get items from Zotero
+
+        :rtype: ``list``
+        """
+        url = self._prep_url("/{t}/{u}/collections/{x}/collections", collection_id)
+        return self._retrieve_data(url, **kwargs)
+
+    def collection_items(self, collection_id, **kwargs):
+        """Get items from Zotero
+
+        :rtype: ``list``
+        """
+        url = self._prep_url("/{t}/{u}/collections/{x}/items", collection_id)
+        return self._retrieve_data(url, **kwargs)
+
+    #TODO: broken
+    def follow(self):
+        """ Return the result of the call to the URL in the 'Next' link
+        """
+        if self.links:
+            url = self.links.get('next')
+            return self._retrieve_data(url)
+        else:
+            return None
+
+    ## -------------------------------------------------------------------------
+
+    ## Full Reference Read API requests  ---------------------------------------
+
+    def item_reference(self, item, **kwargs):
+        info = self.item(item,
+                         include='bib',
+                         **kwargs)
+        return info['bib']
+
+    def tag_references(self, tag_name, **kwargs):
+        info = self.items(include='bib',
+                          tag=tag_name,
+                          itemtype='-attachment || note',
+                          limit=100,
+                          **kwargs)
+        return [x['bib'] for x in info]
+
+    def collection_references(self, collection, **kwargs):
+        info = self.collection_items(collection,
+                                     include='bib',
+                                     itemtype='-attachment || note',
+                                     limit=100,
+                                     **kwargs)
+        return [x['bib'] for x in info]
+
+    ## -------------------------------------------------------------------------
+
+    ## Short Citation Read API requests  ---------------------------------------
+
+    def item_citation(self, item, **kwargs):
+        info = self.item(item,
+                         include='citation',
+                         **kwargs)
+        return info['citation']
 
 #-------------------------------------------------------------------------------
 # :class:`ZotWorkflow` 
@@ -1286,13 +1504,16 @@ class ZotWorkflow(object):
 
         """
         # Set `export` properties
-        self.settings = self.zotero.api_settings
+        self.api = self.zotero.api_settings
         self.prefs = self.zotquery.output_settings
+        self.zot = ZotAPI(library_id=self.api['user_id'],
+                          library_type='user',
+                          api_key=self.api['api_key'])
         # Retrieve HTML of item
         cites = self.get_export_html()
         # Export text of item to appropriate location
         self.send_export_text(cites)
-        return True
+        return self.prefs['fmt']
 
     # `open` paths  ------------------------------------------------------------
 
@@ -1854,9 +2075,9 @@ class ZotWorkflow(object):
         return quickcopy
 
 
-#-------------------------------------------------------------------------------
-### `Export` method
-#-------------------------------------------------------------------------------
+  #-----------------------------------------------------------------------------
+  ### `Export` method
+  #-----------------------------------------------------------------------------
     
     # Retrieve HTML of item  ---------------------------------------------------
 
@@ -1867,14 +2088,12 @@ class ZotWorkflow(object):
         # check if item reference has already been generated and cached
         cached = self.wf.cached_data(self.arg, max_age=0)
         if cached:
+            flag = self.flag if self.flag != 'append' else 'bib'
             # check if item reference is right kind
-            if self.flag in cached.keys():
-                cites = cached[self.flag]
+            if flag in cached.keys():
+                cites = cached[flag]
         # if not exported before
         else:
-            self.zot = zotero.Zotero(self.settings['user_id'], 
-                                     self.settings['user_type'], 
-                                     self.settings['api_key'])
             # Choose appropriate code branch
             if self.flag in ('bib', 'citation', 'append'):
                 cites = self.export_item()
@@ -1908,13 +2127,11 @@ class ZotWorkflow(object):
         """Export individual item in preferred format.
 
         """
-        content = 'bib' if self.flag == 'append' else self.flag
         item_id = self.arg.split('_')[1]
-        cites = self.zot.item(item_id,
-                            content=content,
-                            style=self.prefs['csl'])
-
-        return '\n'.join(cites)
+        cite = self.zot.item(item_id,
+                             include=self.flag,
+                             style=self.prefs['csl'])
+        return cite[self.flag]
 
     ## Export entire `tag` or `collection`  ------------------------------------
 
@@ -1924,14 +2141,12 @@ class ZotWorkflow(object):
         """
         group_type, item_id = self.arg.split('_')
         if group_type == 'c':
-            cites = self.zot.collection_items(item_id,
-                                              content='bib',
-                                              style=self.prefs['csl'])
+            cites = self.zot.collection_references(item_id,
+                                                   style=self.prefs['csl'])
         elif group_type == 't':
             tag_name = self._get_tag_name(item_id)
-            cites = self.zot.tag_items(tag_name,
-                                       content='bib',
-                                       style=self.prefs['csl'])
+            cites = self.zot.tag_references(tag_name,
+                                            style=self.prefs['csl'])
         return '\n'.join(cites)
 
     ### ------------------------------------------------------------------------
